471e6a8ee80c799976eec55e8231bad9f7437da8
IwoSzczepaniak <iwosz@LAPTOP-O8EOC9KI>
2023-03-30 14:05:36 +0200

    lab5_class

---
+++ cw05/lab/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char*argv[])
+{
+    int fd[2];
+    pipe(fd);
+    pid_t pid = fork();
+    if (pid == 0) 
+    {
+        close(fd[1]); 
+        char buffer[100];
+        read(fd[0], buffer, sizeof(buffer));
+        printf("Received data: %s\n", buffer);
+    }
+    else
+    {
+        close(fd[0]);
+        char *message = "Hello world";
+        write(fd[1], message, strlen(message) + 1); 
+    }
+
+}
