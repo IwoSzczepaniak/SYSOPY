22e439f18528c9fc26df3bc622ec44423e2772af
IwoSzczepaniak <iszczepaniak@student.agh.edu.pl>
2023-04-12 23:01:53 +0200

    zad2

--- cw05/zad2/main.c
+++ cw05/zad2/main.c
@@ -17,10 +17,11 @@ int main(int argc, char* argv[])
         return 1;
     }
 
+    clock_t start = clock();
+
     double cell_size = atof(argv[1]);
     int proc_num = atoi(argv[2]);
     int fd[proc_num][2];
-    clock_t start = clock();
 
 
     for (int i = 0; i < proc_num; i++)
@@ -75,7 +76,7 @@ int main(int argc, char* argv[])
 
     FILE *raport =fopen("raport.txt", "a"); 
     fprintf(raport, "Cell size: %s, \t\tNumber of processes: %s", argv[1], argv[2]);
-    fprintf(raport, "Result: %f", sum);
+    fprintf(raport, ", Result: %f", sum);
     fprintf(raport, ", Time: %fs\n", (float)(end - start) / CLOCKS_PER_SEC);
 
     return 0;
