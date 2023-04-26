80db20f3430595cd74979122ad25e363c3f9f8f4
IwoSzczepaniak <iszczepaniak@student.agh.edu.pl>
2023-04-26 16:26:51 +0200

    zad1

---
+++ cw06/zad1/client.c
@@ -0,0 +1,192 @@
+#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/msg.h> 
+#include <sys/ipc.h>
+#include <string.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <time.h>
+#include "common.h"
+
+#define MY_SERVER_ID 1
+#define PATH getenv("HOME")
+
+key_t queueKey;
+int queueID, serverID, clientID;
+
+int init();
+void list();
+void to_all(char* message);
+void to_one(char* message, int receiver);
+void stop_client();
+void handle_server_message();
+
+
+
+int main(void)
+{
+    queueKey = ftok(PATH , getpid());
+    queueID = msgget(queueKey, IPC_CREAT | 0666);
+    key_t serverKey = ftok(PATH, MY_SERVER_ID);
+    serverID = msgget(serverKey, 0600);
+
+
+    clientID = init();
+    size_t len = 0;
+    ssize_t command_size;
+    char* command = NULL;
+
+    signal(SIGINT, stop_client);
+
+    while(1){
+        printf("Type in command > ");
+        command_size = getline(&command, &len, stdin);
+        command[command_size-1] ='\0';
+
+        handle_server_message();
+
+        char *current = strtok(command," ");
+
+        
+        if (strcmp(current, "") == 0) 
+        {
+            continue;
+        }
+        printf("%s\n" , current);
+
+        if(strcmp(current , "LIST") == 0)
+        {
+            list();
+        }
+        else if(strcmp(current , "STOP") == 0)
+        {
+            stop_client();
+        }
+        else if(strcmp(current , "2ONE") == 0)
+        {
+            current = strtok(NULL, " ");
+            int reciever = atoi(current);
+            printf("%d\n" , reciever);
+            current = strtok(NULL, " ");
+
+            to_one(current , reciever);
+        
+        }
+        else if (strcmp(current , "2ALL") == 0)
+        {
+            current = strtok(NULL, " ");
+            to_all(current);
+        }
+        else
+        {
+            printf("Command has not been recognized\n");
+        }
+
+
+       
+    }
+    return 0;
+}
+
+void handle_server_message()
+{
+    CommandBuff *mybuff = malloc(sizeof(CommandBuff));
+
+    if(msgrcv(queueID, mybuff, sizeof(CommandBuff),0,IPC_NOWAIT) == -1 ){
+        perror("msgrcv");
+        exit(EXIT_FAILURE);
+    }
+    if(mybuff->command == 4)
+    {
+    printf("STOP command has been recived , exiting\n");
+    stop_client();
+    }
+    else
+    {
+        struct tm tmp_time = mybuff -> time_struct;
+        printf("Client %d has sent a message at %02d:%02d:%02d \n", mybuff->client_ID,tmp_time.tm_hour,tmp_time.tm_min,tmp_time.tm_sec);
+        printf("Message : %s\n" , mybuff->message);
+    }
+    
+}
+
+int init()
+{
+    CommandBuff *mybuff = malloc(sizeof(CommandBuff));
+
+    time_t current_time = time(NULL);
+    mybuff->time_struct = *localtime(&current_time);
+    mybuff->command = 5;
+    mybuff->queue_Key = queueKey;
+    msgsnd(serverID, mybuff, sizeof(CommandBuff),0);
+
+    msgrcv(queueID, mybuff, sizeof(CommandBuff),0,0);
+    int current_id = mybuff->client_ID;
+
+    printf("My client ID is %d\n" , current_id);
+
+    return current_id;
+}
+
+
+
+void list()
+{
+    time_t current_time = time(NULL);
+
+    CommandBuff *mybuff = malloc(sizeof(CommandBuff));
+
+    mybuff->time_struct = *localtime(&current_time);
+    mybuff->client_ID = clientID;
+    mybuff->command = 1;
+
+    msgsnd(serverID, mybuff, sizeof(CommandBuff),0);
+    msgrcv(queueID, mybuff, sizeof(CommandBuff),0,0);
+}
+
+void to_all(char* message)
+{
+    time_t current_time = time(NULL);
+
+    CommandBuff *mybuff = malloc(sizeof(CommandBuff));
+
+    mybuff->time_struct = *localtime(&current_time);
+    mybuff->client_ID = clientID;
+    mybuff->command = 2;
+    strcpy(mybuff->message, message);
+
+    msgsnd(serverID, mybuff, sizeof(CommandBuff),0);
+}
+
+void to_one(char* message, int receiver)
+{
+    time_t current_time = time(NULL);
+
+    CommandBuff *mybuff = malloc(sizeof(CommandBuff));
+
+    mybuff->time_struct = *localtime(&current_time);
+    mybuff->client_ID = clientID;
+    mybuff->receiver_ID = receiver;
+    mybuff->command = 3;
+    strcpy(mybuff->message, message);
+    msgsnd(serverID, mybuff, sizeof(CommandBuff),0);
+}
+
+
+void stop_client()
+{
+    CommandBuff *mybuff = malloc(sizeof(CommandBuff));
+
+    time_t current_time = time(NULL);
+    mybuff->time_struct = *localtime(&current_time);
+    mybuff->client_ID = clientID;
+    mybuff->command = 4;
+
+    msgsnd(serverID, mybuff, sizeof(CommandBuff),0);
+    msgctl(queueID, IPC_RMID, NULL);
+
+    exit(0);
+
+}
+
---
+++ cw06/zad1/server.c
@@ -0,0 +1,207 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/types.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <time.h>
+#include <fcntl.h>
+#include "common.h"
+
+#define MAX_CLIENTS 4
+#define MY_SERVER_ID 1
+#define PATH getenv("HOME")
+
+int server_queue, new_client_ID;
+key_t client_queue[MAX_CLIENTS];
+
+void stop_server();
+void init(CommandBuff *mybuff);
+void list(CommandBuff *mybuff);
+void to_all(CommandBuff *mybuff);
+void to_one(CommandBuff *mybuff);
+void stop_client(CommandBuff *mybuff);
+void save_command(CommandBuff *mybuff);
+
+int main() 
+{
+    printf("Server is starting\n");
+
+    for (int i = 0 ; i < MAX_CLIENTS; i++){
+        client_queue[i]=-1;
+    }
+
+    key_t queueKey = ftok(PATH, MY_SERVER_ID);
+    CommandBuff *mybuff = malloc(sizeof(CommandBuff));
+    server_queue = msgget(queueKey, IPC_CREAT | 0666);
+    if (server_queue == -1) {
+        perror("msgget");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server started, queue ID: %d\n", server_queue);
+
+    signal(SIGINT, stop_server);
+
+    while (1) {
+
+        if (msgrcv(server_queue, &mybuff, sizeof(mybuff), -6, 0) == -1) {
+            perror("msgrcv");
+            exit(EXIT_FAILURE);
+        }
+
+        switch (mybuff->command) 
+        {
+            case 5: 
+            {
+                printf("Init command has been recieved\n");
+                init(mybuff);
+            }
+            case 1:
+            {
+                printf("List command has been recieved\n");
+                list(mybuff);
+            }
+            case 2: 
+            {
+                printf("To all command has been recieved\n");
+                to_all(mybuff);
+            }
+            case 3: 
+            {
+                printf("To one command has been recieved\n");
+                to_one(mybuff);
+            }
+            case 4: 
+            {
+                printf("Stop(client) command has been recieved\n");
+                stop_client(mybuff);
+            }
+            default: 
+            {
+                // If the message type is not recognized, send an error message back to the sender
+                break;
+            }
+        }
+
+
+    }
+    return 0;
+}
+
+void stop_server()
+{
+    CommandBuff *mybuff = malloc(sizeof(CommandBuff));
+
+    for (int i = 0 ; i < MAX_CLIENTS; i++){
+        if (client_queue[i] != -1){
+            mybuff->command = 4;
+            int client_queueID = msgget(client_queue[i],0);
+            msgsnd(client_queueID, mybuff,sizeof(CommandBuff),0);
+            msgrcv(server_queue, mybuff,sizeof(CommandBuff),5,0);
+        }
+
+    }
+
+    msgctl(server_queue, IPC_RMID, NULL);
+    exit(0);
+}
+
+void save_command(CommandBuff *mybuff)
+{
+    struct tm tmp_time = mybuff->time_struct;
+    FILE *file = fopen("command_history.txt", "a");
+
+    switch (mybuff->command){
+        case 5:
+        {
+            if(mybuff->client_ID == -1){
+                printf("Client limit has been reached\n");
+            }else{
+                printf("Server : New client has been initialized - Client ID: %d\n" ,mybuff->client_ID );
+                fprintf(file, "Server : New client has been initialized - Client ID: %d\n", mybuff->client_ID);
+            }
+        }
+        case 1:
+        {
+            fprintf(file, "Server : LIST command - Client ID: %d\n", mybuff->client_ID);
+        }
+        case 2:
+        {
+            fprintf(file, "Server : 2ALL command - Client ID: %d\n", mybuff->client_ID);
+            fprintf(file, "Message: %s\n", mybuff->message);
+        }
+        case 3:
+        {
+            fprintf(file, "Server : 2ONE command - Client ID: %d receiver ID: %d\n", mybuff->client_ID , mybuff->receiver_ID);
+            fprintf(file, "Message: %s\n", mybuff->message);
+        }
+        case 4:
+        {
+            fprintf(file, "Server : STOP command - Client ID: %d\n", mybuff->client_ID);
+
+        }
+    }
+    fprintf(file, "sent at: %02d:%02d:%02d\n\n",tmp_time.tm_hour,tmp_time.tm_min,tmp_time.tm_sec);
+
+    fclose(file);
+
+}
+
+void init(CommandBuff *mybuff)
+{
+    while(client_queue[new_client_ID] != -1 && (new_client_ID < MAX_CLIENTS - 1)){
+        new_client_ID++;
+    }
+
+    if (new_client_ID < MAX_CLIENTS - 1){
+        mybuff->client_ID = new_client_ID;
+        client_queue[new_client_ID] = mybuff->queue_Key;
+    }
+
+    int client_queueID = msgget(mybuff->queue_Key,0600);
+    msgsnd(client_queueID, mybuff, sizeof(CommandBuff),0);
+
+    save_command(mybuff);
+}
+
+
+void list(CommandBuff *mybuff){
+
+    for (int i = 0; i < MAX_CLIENTS; i++){
+            if(client_queue[i]!=-1){
+                printf("Client id: %d\n", i);
+            }
+        }
+    int client_queueID = msgget(mybuff->queue_Key,0600);
+    msgsnd(client_queueID, mybuff, sizeof (CommandBuff), 0);
+    
+    save_command(mybuff);
+}
+
+void to_all(CommandBuff *mybuff){
+    for(int i =0 ; i < MAX_CLIENTS ; i++){
+        if(client_queue[i] != -1 && mybuff->client_ID != i){
+            int receiverqueueID = msgget(client_queue[i],0);
+            msgsnd(receiverqueueID,mybuff,sizeof(CommandBuff),0);
+        }
+    }
+    save_command(mybuff);
+}
+
+void to_one(CommandBuff *mybuff){
+    int receiverqueueID = msgget(client_queue[mybuff->receiver_ID],0);
+    msgsnd(receiverqueueID,mybuff,sizeof(CommandBuff),0);
+    save_command(mybuff);
+}
+
+void stop_client(CommandBuff *mybuff){
+    client_queue[mybuff->client_ID] = -1;
+
+    if(mybuff->client_ID < new_client_ID){
+        new_client_ID = mybuff->client_ID;
+    }
+    save_command(mybuff);
+}
